43894b41b8d5aff3100ae7abea255c76ce0bae9b
MatePatrik <matepatrik2002@gmail.com>
2024-04-24 10:32:44 +0200

    Add project files.

---
+++ DolgozokConsoleApp/Adatbazis.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DolgozokConsoleApp
+{
+    internal class Adatbazis
+    {
+        MySqlCommand sqlCommand;
+        MySqlConnection sqlConnection;
+
+        public Adatbazis()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "";
+            builder.Database = "dolgozok";
+            sqlConnection = new MySqlConnection(builder.ConnectionString);
+            sqlCommand = sqlConnection.CreateCommand();
+            try
+            {
+                kapcsolatNyit();
+                kapcsolatZar();
+            }
+            catch (MySqlException sql)
+            {
+                Console.WriteLine(sql.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+
+        private void kapcsolatZar()
+        {
+            if (sqlConnection.State != System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void kapcsolatNyit()
+        {
+            if (sqlConnection.State != System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        internal List<Dolgozo> getAllDolgozo()
+        {
+            List<Dolgozo> dolgozok = new List<Dolgozo>();
+            sqlCommand.CommandText = "SELECT `nev`, `neme`, `reszleg`, `belepesev`, `ber` FROM `dolgozok` WHERE 1";
+            kapcsolatNyit();
+            using (MySqlDataReader dr = sqlCommand.ExecuteReader()) 
+            {
+                while (dr.Read())
+                {
+                    Dolgozo dolgozo = new Dolgozo(dr.GetString("nev"), dr.GetString("neme"), dr.GetString("reszleg"), dr.GetInt32("belepesev"), dr.GetInt32("ber"));
+                    dolgozok.Add(dolgozo);
+                }
+                return dolgozok;
+            }
+        }
+    }
+}
---
+++ DolgozokConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozokConsoleApp
+{
+    internal class Program
+    {
+        static Adatbazis db = new Adatbazis();
+        static List<Dolgozo> dolgozok;
+        static void Main(string[] args)
+        {
+            dolgozok = db.getAllDolgozo();
+            Feladat1();
+            Feladat2();
+            Feladat3();
+            Feladat4();
+            Console.WriteLine("Program vége!");
+            Console.ReadLine();
+        }
+
+        private static void Feladat4()
+        {
+            Console.WriteLine("4.feladat:");
+            foreach (var dolgozo in dolgozok.Where(x => x.reszleg == "asztalosműhely"))
+            {
+                Console.WriteLine($"\t{dolgozo.nev}.");
+            }
+        }
+
+        private static void Feladat3()
+        {
+            Console.WriteLine("3.feladat:");
+            foreach (var dolgozo in dolgozok.GroupBy(x => x.reszleg).Select(y => new {reszleg = y.Key, letszam = y.Count()}))
+            {
+                Console.WriteLine($"\t{dolgozo.reszleg} {dolgozo.letszam}.");
+            }
+        }
+
+        private static void Feladat2()
+        {
+            Console.WriteLine("2.feladat:");
+            int MaxBer = dolgozok.Max(x => x.ber);
+            Console.WriteLine($"\tA legnagyobb bérel rendelkező: {dolgozok.First(x => x.ber == MaxBer).nev}.");
+        }
+
+        private static void Feladat1()
+        {
+            Console.WriteLine("1.feladat:");
+            Console.WriteLine($"\tA dolgozók száma: {dolgozok.Count} fő.");
+        }
+    }
+}
